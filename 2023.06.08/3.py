a55e639ae1a289bf3e46df0ec203519a2bf539e6
mrkaramyshev <karamyshev102@mail.ru>
2023-06-17 15:30:11 +0500

    Третье задание

---
+++ 2023.06.08/3.py
@@ -0,0 +1,10 @@
+def numbers_strip(numbers: list[float], n: int = 1, return_copy: bool = False) -> list[float]:
+    if return_copy:
+        numbers = numbers.copy()
+
+    for _ in range(n):
+        if numbers:
+            numbers.remove(min(numbers))
+            numbers.remove(max(numbers))
+    
+    return numbers
