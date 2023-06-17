f1fbee2189dc99a36d6ccb3d319cdba38acbd18b
mrkaramyshev <karamyshev102@mail.ru>
2023-06-17 16:02:34 +0500

    пятое

---
+++ 2023.06.08/5.py
@@ -0,0 +1,25 @@
+def central_tendency(num1: float, num2: float, *numbers: float) -> dict[str, float]:
+    all_numbers = [num1, num2, *numbers]
+    sorted_numbers = sorted(all_numbers)
+    length = len(sorted_numbers)
+
+    if length % 2 == 0:
+        median = (sorted_numbers[length // 2 - 1] + sorted_numbers[length // 2]) / 2
+    else:
+        median = sorted_numbers[length // 2]
+
+    arithmetic_mean = sum(sorted_numbers) / length
+
+    geometric_mean = 1
+    for number in sorted_numbers:
+        geometric_mean *= number
+    geometric_mean = geometric_mean ** (1 / length)
+
+    harmonic_mean = length / sum(1 / number for number in sorted_numbers)
+
+    return {
+        'median': median,
+        'arithmetic': arithmetic_mean,
+        'geometric': geometric_mean,
+        'harmonic': harmonic_mean
+    }
---
+++ 2023.06.08/6.py
@@ -0,0 +1,13 @@
+def orth_triangle(*, cathetus1: float = None, cathetus2: float = None, hypotenuse: float = None) -> float:
+    if cathetus1 is not None and cathetus2 is not None:
+        return (cathetus1 ** 2 + cathetus2 ** 2) ** 0.5
+    elif cathetus1 is not None and hypotenuse is not None:
+        if cathetus1 > hypotenuse:
+            return None
+        return (hypotenuse ** 2 - cathetus1 ** 2) ** 0.5
+    elif cathetus2 is not None and hypotenuse is not None:
+        if cathetus2 > hypotenuse:
+            return None
+        return (hypotenuse ** 2 - cathetus2 ** 2) ** 0.5
+    else:
+        return None
